a2bc8f5ed911d550e3dd48f5da921af5768ad412
mudassirudd <mudassirudd@gmail.com>
2025-10-07 09:56:14 +0530

    removed scroll indicator

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -147,25 +147,7 @@ export function Hero() {
             </motion.div>
           </motion.div>
 
-          {/* Scroll indicator */}
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.6, delay: 1 }}
-            className="absolute bottom-8 left-1/2 -translate-x-1/2"
-          >
-            <motion.div
-              animate={{ y: [0, 10, 0] }}
-              transition={{ duration: 2, repeat: Infinity }}
-              className="w-6 h-10 border-2 border-muted-foreground/50 rounded-full flex items-start justify-center p-2"
-            >
-              <motion.div
-                animate={{ y: [0, 12, 0] }}
-                transition={{ duration: 2, repeat: Infinity }}
-                className="w-1.5 h-1.5 bg-muted-foreground/50 rounded-full"
-              />
-            </motion.div>
-          </motion.div>
+         
         </div>
       </div>
       {/* Bottom gradient fade */}
